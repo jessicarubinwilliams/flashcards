82b33429ba07f710a22765a5148cb46b9c0c67d2
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-01-25 13:43:14 -0800

    Change background color of #operator-definition to test that selector is working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $("p#operator").click(function() {
-    $(".definitions").hide();
-    $("#operator-definition").show();
+    // $(".definitions").hide();
+    // $("#operator-definition").show();
+    $("#operator-definition").css("background-color", "green");
   });
 });
